603e82b4a9747ed7f412fcef7faa42651b8b3678
Vadim Pimenov <zigmundfreid89@yandex.ru>
2025-04-06 01:26:38 +0300

    Закончить функциональность контроллеров Film и User с валидацией без Spring валидации @Valide

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,149 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+    // таблица для хранения фильмов
+    Map<Long, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> getAll() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create(@RequestBody Film film) {
+        log.info("Добавление фильма. {}", film);
+        validateForCreate(film);
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        log.info("Фильм с id {} успешно добавлен.", film.getId());
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@RequestBody Film newFilm) {
+        Long id = newFilm.getId();
+        if (Objects.isNull(id)) {
+            String message = "id должен быть указан.";
+            log.warn(message);
+            throw new ValidationException(message);
+        }
+        if (!films.containsKey(id)) {
+            String message = String.format("Фильм с id %d, не найден.", newFilm.getId());
+            log.warn(message);
+            throw new NotFoundException(message);
+        }
+
+        Film oldFilm = films.get(newFilm.getId());
+
+        if (Objects.nonNull(newFilm.getName())) {
+            if(!isValidName(newFilm)) {
+                nameException();
+            }
+            oldFilm.setName(newFilm.getName());
+        }
+        if (Objects.nonNull(newFilm.getDescription())) {
+            if(!isValidDescription(newFilm)) {
+                descriptionException();
+            }
+            oldFilm.setDescription(newFilm.getDescription());
+        }
+        if (Objects.nonNull(newFilm.getReleaseDate())) {
+            if(!isValidRelease(newFilm)) {
+                releaseException();
+            }
+            oldFilm.setReleaseDate(newFilm.getReleaseDate());
+        }
+        if (Objects.nonNull(newFilm.getDuration())) {
+            if(!isValidDuration(newFilm)) {
+                durationException();
+            }
+            oldFilm.setDuration(newFilm.getDuration());
+        }
+        log.info("Данные фильма с id {} успешны обновлены.", oldFilm.getId());
+        return oldFilm;
+    }
+
+
+    private Long getNextId() {
+        long nextId = films.keySet().stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++nextId;
+    }
+
+    private void validateForCreate(Film film) {
+        if (!isValidName(film)) {
+            nameException();
+        }
+        if (!isValidDescription(film)) {
+            descriptionException();
+        }
+        if (!isValidRelease(film)) {
+            releaseException();
+        }
+        if (!isValidDuration(film)) {
+            durationException();
+        }
+        log.info("Фильм {} прошел валидацию.", film);
+    }
+
+    private void nameException() {
+        String message = "Название должно быть заполнено.";
+        log.warn(message);
+        throw new ValidationException(message);
+    }
+
+    private void descriptionException() {
+        String message = "Описание не должно превышать 200 символов.";
+        log.warn(message);
+        throw new ValidationException(message);
+    }
+
+    private void releaseException() {
+        String message = "Дата создания указана неверно.";
+        log.warn(message);
+        throw new ValidationException(message);
+    }
+
+    private void durationException() {
+        String message = "Продолжительность должна быть больше 0.";
+        log.warn(message);
+        throw new DuplicatedDataException(message);
+    }
+
+
+    private boolean isValidName(Film film) {
+        return Objects.nonNull(film.getName());
+    }
+
+    private boolean isValidDescription(Film film) {
+        int numberOfChar = 200;
+        return (Objects.isNull(film.getDescription())) ? true : film.getDescription().length() <= numberOfChar;
+    }
+
+    private boolean isValidRelease(Film film) {
+        return (Objects.isNull(film.getReleaseDate())) ? true : film.getReleaseDate().isAfter(LocalDate.of(1895, 12, 28));
+    }
+
+    private boolean isValidDuration(Film film) {
+        return (Objects.isNull(film.getDuration())) ? true : film.getDuration() > 0;
+    }
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,4 +1,153 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
 public class UserController {
+
+    // Таблица для хранения наших пользователей по email
+    Map<Long, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> getAll() {
+        log.info("Список пользователей успешно отправлен");
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        log.info("Добавление пользователя {}.", user);
+        validateToCreate(user);
+        user.setId(getNextId());
+        users.put(user.getId(), user);
+        log.info("Пользователь успешно добавлен в список");
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User newUser) {
+        Long id = newUser.getId();
+        if (Objects.isNull(id)) {
+            String message = "id должен быть указан.";
+            log.warn(message);
+            throw new ValidationException(message);
+        }
+        if (!users.containsKey(id)) {
+            String message = String.format("Пользователь с id %d, не найден.", newUser.getId());
+            log.warn(message);
+            throw new NotFoundException(message);
+        }
+
+        User oldUser = users.get(newUser.getId());
+
+        if (Objects.nonNull(newUser.getEmail())) {
+            if (!isValidEmail(newUser)) {
+                emailException();
+            }
+            oldUser.setEmail(newUser.getEmail());
+        }
+        if (Objects.nonNull(newUser.getLogin())) {
+            if (!isValidLogin(newUser)) {
+                loginException();
+            }
+            oldUser.setLogin(newUser.getLogin());
+        }
+        if (Objects.nonNull(newUser.getBirthday())) {
+            if (!isValidBirthDay(newUser)) {
+                birthDayException();
+            }
+            oldUser.setBirthday(newUser.getBirthday());
+        }
+        if (Objects.nonNull(newUser.getName())) {
+            oldUser.setName(newUser.getName());
+        }
+
+        users.put(id, oldUser);
+        log.info("Данные пользователя с id {} успешно обновлены.", id);
+        return oldUser;
+    }
+
+    // метод для получения id, получаем максимальный ключ-id нашей таблицы пользователей, увеличиваем его на 1
+    // если это первый ключ, то записываем значение 1
+    private Long getNextId() {
+        long nextId = users.keySet().stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++nextId;
+    }
+
+    private void validateToCreate(User user) {
+        if (!isValidEmail(user)) {
+            emailException();
+        }
+        if (!isValidLogin(user)) {
+            loginException();
+        }
+        if (!isValidBirthDay(user)) {
+            birthDayException();
+        }
+        if (users.containsValue(user)) {
+            duplicateEmailException();
+        }
+        if (Objects.isNull(user.getName())) {
+            log.info("Пользователь с id не указал, установка логина в качестве имени.");
+            user.setName(user.getLogin());
+        }
+        log.info("Валидация пользователя с id {} прошла успешна. ", user.getId());
+    }
+
+    private void emailException() {
+        String message = "Имейл не указан или некорректен.";
+        log.warn(message);
+        throw new ValidationException(message);
+    }
+
+    private void loginException() {
+        String message = "Логин должен быть указан и не должен содержать пробелы.";
+        log.warn(message);
+        throw new ValidationException(message);
+    }
+
+    private void birthDayException() {
+        String message = "Дата рождения указана неверно.";
+        log.warn(message);
+        throw new ValidationException(message);
+    }
+
+    private void duplicateEmailException() {
+        String message = "Этот имейл уже используется";
+        log.warn(message);
+        throw new DuplicatedDataException(message);
+    }
+
+
+    private boolean isValidBirthDay(User user) {
+        if (Objects.isNull(user.getBirthday())) {
+            return true;
+        }
+        return user.getBirthday().isBefore(LocalDate.now());
+    }
+
+    private boolean isValidLogin(User user) {
+        return Objects.nonNull(user.getLogin()) && !user.getLogin().contains(" ");
+    }
+
+    private boolean isValidEmail(User user) {
+        return Objects.nonNull(user.getEmail()) && !user.getEmail().isBlank() && user.getEmail().contains("@");
+    }
+
 }
