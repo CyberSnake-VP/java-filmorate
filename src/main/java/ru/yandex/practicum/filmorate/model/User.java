46ded57a785fdb734c8620ec227efcee1ef242d7
Vadim Pimenov <zigmundfreid89@yandex.ru>
2025-04-07 02:06:31 +0300

    Закончить обработчик UserController с валидацией анотациями

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,16 +1,26 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-
 import java.time.LocalDate;
 
 @Data
-@EqualsAndHashCode(of = "email")
 public class User {
     private Long id;
+
+    @NotEmpty(message = "Email должен быть указан.")
+    @Email(message = "Email не заполнен или не корректен.")
     private String email;
+
+
+    @NotEmpty(message = "Логин должен быть заполнен.")
+    @Pattern(regexp = "\\S+", message = "Логин не должен содержать пробелы.")
     private String login;
+
     private String name;
+
+    @Past(message = "Дата рождения не может быть в будущем.")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 }
