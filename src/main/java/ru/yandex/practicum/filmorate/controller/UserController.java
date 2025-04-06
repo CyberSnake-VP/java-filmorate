18f936317eb7b22d65bd0aa66a3155274d8241ed
Vadim Pimenov <zigmundfreid89@yandex.ru>
2025-04-07 02:06:31 +0300

    Закончить обработчик UserController с валидацией анотациями

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,12 +2,11 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
+
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,29 +15,31 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/users")
 @Slf4j
-public class UserController {
+public class UserController implements Controller {
 
     // Таблица для хранения наших пользователей по email
     Map<Long, User> users = new HashMap<>();
 
-    @GetMapping
+    @Override
     public Collection<User> getAll() {
         log.info("Список пользователей успешно отправлен");
         return users.values();
     }
 
-    @PostMapping
-    public User create(@RequestBody User user) {
+    @Override
+    public User create(User user) {
         log.info("Добавление пользователя {}.", user);
-        validateToCreate(user);
         user.setId(getNextId());
+        if(Objects.isNull(user.getName()) || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
         users.put(user.getId(), user);
         log.info("Пользователь успешно добавлен в список");
         return user;
     }
 
-    @PutMapping
-    public User update(@RequestBody User newUser) {
+    @Override
+    public User update(User newUser) {
         Long id = newUser.getId();
         if (Objects.isNull(id)) {
             String message = "id должен быть указан.";
@@ -53,28 +54,18 @@ public class UserController {
 
         User oldUser = users.get(newUser.getId());
 
-        if (Objects.nonNull(newUser.getEmail())) {
-            if (!isValidEmail(newUser)) {
-                emailException();
-            }
+        if(newUser.getEmail() != null) {
             oldUser.setEmail(newUser.getEmail());
         }
-        if (Objects.nonNull(newUser.getLogin())) {
-            if (!isValidLogin(newUser)) {
-                loginException();
-            }
+        if(newUser.getLogin() != null) {
             oldUser.setLogin(newUser.getLogin());
         }
-        if (Objects.nonNull(newUser.getBirthday())) {
-            if (!isValidBirthDay(newUser)) {
-                birthDayException();
-            }
-            oldUser.setBirthday(newUser.getBirthday());
-        }
-        if (Objects.nonNull(newUser.getName())) {
+        if(newUser.getName() != null) {
             oldUser.setName(newUser.getName());
         }
-
+        if(newUser.getBirthday() != null) {
+            oldUser.setBirthday(newUser.getBirthday());
+        }
         users.put(id, oldUser);
         log.info("Данные пользователя с id {} успешно обновлены.", id);
         return oldUser;
@@ -89,65 +80,4 @@ public class UserController {
                 .orElse(0);
         return ++nextId;
     }
-
-    private void validateToCreate(User user) {
-        if (!isValidEmail(user)) {
-            emailException();
-        }
-        if (!isValidLogin(user)) {
-            loginException();
-        }
-        if (!isValidBirthDay(user)) {
-            birthDayException();
-        }
-        if (users.containsValue(user)) {
-            duplicateEmailException();
-        }
-        if (Objects.isNull(user.getName())) {
-            log.info("Пользователь с id не указал, установка логина в качестве имени.");
-            user.setName(user.getLogin());
-        }
-        log.info("Валидация пользователя с id {} прошла успешна. ", user.getId());
-    }
-
-    private void emailException() {
-        String message = "Имейл не указан или некорректен.";
-        log.warn(message);
-        throw new ValidationException(message);
-    }
-
-    private void loginException() {
-        String message = "Логин должен быть указан и не должен содержать пробелы.";
-        log.warn(message);
-        throw new ValidationException(message);
-    }
-
-    private void birthDayException() {
-        String message = "Дата рождения указана неверно.";
-        log.warn(message);
-        throw new ValidationException(message);
-    }
-
-    private void duplicateEmailException() {
-        String message = "Этот имейл уже используется";
-        log.warn(message);
-        throw new DuplicatedDataException(message);
-    }
-
-
-    private boolean isValidBirthDay(User user) {
-        if (Objects.isNull(user.getBirthday())) {
-            return true;
-        }
-        return user.getBirthday().isBefore(LocalDate.now());
-    }
-
-    private boolean isValidLogin(User user) {
-        return Objects.nonNull(user.getLogin()) && !user.getLogin().contains(" ");
-    }
-
-    private boolean isValidEmail(User user) {
-        return Objects.nonNull(user.getEmail()) && !user.getEmail().isBlank() && user.getEmail().contains("@");
-    }
-
 }
